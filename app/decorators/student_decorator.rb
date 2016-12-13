5a802401a8b35241e057f13442de646b7a9af002
vento <marcin.vento@gmail.com>
2016-12-13 18:15:33 +0100

    Add Methods to Student and Teacher Decorator Files

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -1,8 +1,18 @@
 class StudentDecorator < BaseDecorator
+  include ActiveSupport::NumberHelper
+  
   def full_name
     "#{first_name} #{last_name}"
   end
 
   def avg_notes(subject_item)
+  	
+  	@avg_notes = subject_item.subject_item_notes.average(:value)
+
+  	if @avg_notes.nil?
+  		number_to_rounded(0, precision: 2)
+  	else
+ 		number_to_rounded(@avg_notes, precision: 2)
+ 	end
   end
 end
